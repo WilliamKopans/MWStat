ad2cf0d0ba000e227e069b48edbe6b808b6ff082
William <wkopans123@gmail.com>
2021-07-21 13:22:55 -0400

    Adding LModel2

--- R/FinalMWStat.R
+++ R/FinalMWStat.R
@@ -28,33 +28,36 @@ library(lmodel2)
 MwStatistics <- function (dat = NULL,TimeVarCol = 1, ColA = 2, ColB = 3, win = NULL, stat = NULL, overlap = 0.0, genplot = TRUE, genDataFrame = FALSE)
 {
   old <- Sys.time() #used to find how long the function takes to run
-  
+
   if (overlap > win) {
     stop("Overlap is larger than window size.")
   }
-  
+  if (win == overlap) {
+    stop("Overlap is equal to window size.")
+  }
+
   data = dat %>% #Selecting the columns
     select(all_of(TimeVarCol),all_of(ColA),all_of(ColB))
-  
+
   StatRec <- data.frame(Value = 1) #creating a dataframe to store the recorded statistics
   TimeVarColRec <- data.frame(Value = 1) #creating a dataframe to store the timeframe
   #It starts with the first cell being one, but that is later removed
-  
+
   data %>% filter(!is.na(TimeVarCol)) #removing any NAs
   data %>% filter(!is.na(ColA))
   data %>% filter(!is.na(ColB))
-  
+
   NewWinStart <- min(data[,1]) #creating the first window
   NewWinEnd <- NewWinStart + win
-  
+
   TestCase <- data %>% #creating the dataframe TestCase which is where the window is stored
     filter(between(as.double(data[,1]),0,0)) #contains nothing at first
-  
+
   while (NewWinEnd < max(data[,1])) { #will loop until the end of the dataset
-    
+
     TestCase <- data %>% #sets up window
       filter(between(as.double(data[,1]),as.double(NewWinStart),as.double(NewWinEnd))) #selecting rows that the TimeVarCol falls within the window range
-    
+
     if(genDataFrame == T){
       if (nrow(TestCase) == 0) {
         print(paste0("Window spanning from ",NewWinStart," to ", NewWinEnd, " contains no values"))
@@ -62,10 +65,10 @@ MwStatistics <- function (dat = NULL,TimeVarCol = 1, ColA = 2, ColB = 3, win = N
     }
     #print(paste0("Window spans from ",NewWinStart," to ", NewWinEnd)) #this can be commented out but will say where your windows are
     #mostly used during testing
-    
+
     if (stat == "Correlation"){
       StatRec[nrow(StatRec) + 1,] = c (cor(TestCase[2],TestCase[3]))
-    } 
+    }
     else if (stat == "P-Value") {
       StatRec[nrow(StatRec) + 1,] = c(t.test(TestCase[,2],TestCase[,3])[3])
     }
@@ -84,14 +87,29 @@ MwStatistics <- function (dat = NULL,TimeVarCol = 1, ColA = 2, ColB = 3, win = N
     else if (stat == "Intercept") {
       StatRec[nrow(StatRec) + 1,] = c ((lm(formula = TestCase[,2] ~ TestCase[,3])$coefficients)[1])
     }
-    else if (stat == "Slope") { 
+    else if (stat == "Slope") {
       StatRec[nrow(StatRec) + 1,] = c ((lm(formula = TestCase[,2] ~ TestCase[,3])$coefficients)[2])
-    } 
+    }
     else if (stat == "OLS") { # ordinary least squares (reduced major axis regression)
-      lmodel2::lmodel2(formula, data = NULL, range.y=NULL, range.x=NULL, nperm=0)
-    } 
+
+#Regression:
+      StatRec[nrow(StatRec) + 1,1] = c (lmodel2::lmodel2(TestCase[,2] ~ TestCase[,3], data = TestCase, range.y=NULL, range.x=NULL, nperm=99)[[3]][2,2]) #Intercept
+      StatRec[nrow(StatRec),2] = c (lmodel2::lmodel2(TestCase[,2] ~ TestCase[,3], data = TestCase, range.y=NULL, range.x=NULL, nperm=99)[[3]][2,3]) #Slope
+      StatRec[nrow(StatRec),3] = c (lmodel2::lmodel2(TestCase[,2] ~ TestCase[,3], data = TestCase, range.y=NULL, range.x=NULL, nperm=99)[[3]][2,4]) #Angle
+      StatRec[nrow(StatRec),4] = c (lmodel2::lmodel2(TestCase[,2] ~ TestCase[,3], data = TestCase, range.y=NULL, range.x=NULL, nperm=99)[[3]][2,5]) #P-Perm (1-tailed)
+
+#Confidence Intervals
+      StatRec[nrow(StatRec),5] = c (lmodel2::lmodel2(TestCase[,2] ~ TestCase[,3], data = TestCase, range.y=NULL, range.x=NULL, nperm=99)[[3]][1,][2]) #OLS intercept
+      StatRec[nrow(StatRec),6] = c (lmodel2::lmodel2(TestCase[,2] ~ TestCase[,3], data = TestCase, range.y=NULL, range.x=NULL, nperm=99)[[3]][1,][3]) #OLS Slope
+      StatRec[nrow(StatRec),7] = c (lmodel2::lmodel2(TestCase[,2] ~ TestCase[,3], data = TestCase, range.y=NULL, range.x=NULL, nperm=99)[[3]][1,][4]) # OLS Angle (Degrees)
+
+
+      #lmodel2(DummyData[,1] ~ DummyData[,2], data = DummyData, range.y=NULL, range.x=NULL, nperm=99)[[4]][1,][2]
+      #MwStatistics(dat = DummyData, win = 2, TimeVarCol = 1, ColA = 2, ColB = 3, stat = "OLS", genplot = T, genDataFrame = T, overlap = 0.5)
+
+    }
     else {
-      print("Please choose a statistic")
+      message("Please choose a statistic")
       break
     }
     TimeVarColRec[nrow(TimeVarColRec) + 1,] = c (colMeans(TestCase[1]))
@@ -101,32 +119,40 @@ MwStatistics <- function (dat = NULL,TimeVarCol = 1, ColA = 2, ColB = 3, win = N
       NewWinStart <- NewWinStart + (win - overlap) #Move a window but backtrack to retain part of previous window
     }
     NewWinEnd <- win + NewWinStart #Window start already dealt with overlap so just adding the window
-    
+
   }
+
+
   RunTime <- Sys.time() - old # calculate difference between start and end times
   RunTime <- round(as.double(sub("", "", RunTime)), digits = 2) #rounds time spent
   print(paste0("Finished in ", RunTime, " Seconds.")) #prints how long the program took to run
-  
+
   StatRec <- StatRec[-1,] #removing that first 1 value
   TimeVarColRec <- TimeVarColRec[-1,] #removing that first 1 value
   #print(summary(StatRec)) #printing a summary of the staristics calculated, can be commented out
-  
-  StatRec <- cbind(TimeVarColRec, StatRec) #Combins the recorded statistics and the window position.
+
+  StatRec <- cbind(TimeVarColRec, StatRec) #Combines the recorded statistics and the window position.
   StatRecDF <- as.data.frame(StatRec) #makes it a dataframe
-  
+
+  if (stat == "OLS") {
+
+    #names(StatRecDF)[2] <- " ";
+
+  }
+
   if (genDataFrame == T){ #If you want to view the dataframe here is your option!
     View(StatRecDF) #This is great if you don't want to rely solely on your eyes and the graph
-  } 
-  
-  if (genplot == T) { #Generates the plot
+  }
+
+  if (genplot == T && stat != "OLS") { #Generates the plot
     ggplot2::ggplot(data=StatRecDF, mapping = ggplot2::aes(x=TimeVarColRec,y=StatRec))+
       ggplot2::geom_line(color = "#0e4142")+
-      ggplot2::labs(y = stat,x="Time Variable",title = "Moving Window Analysis") 
-    #If you want to be able to share the plot directly, 
-    #I can have the type of time variable as an input 
+      ggplot2::labs(y = stat,x="Time Variable",title = "Moving Window Analysis")
+    #If you want to be able to share the plot directly,
+    #I can have the type of time variable as an input
     #to the function which is filled in on the X-Axis rather than "Time Varaible"
   }
-  
+
 }
 
 #Examples for each statistic:
@@ -138,3 +164,5 @@ MwStatistics <- function (dat = NULL,TimeVarCol = 1, ColA = 2, ColB = 3, win = N
 #MwStatistics(dat = TotalPiecewiseInterp, win = 2, TimeVarCol = 1, ColA = 2, ColB = 3, stat = "Intercept", genplot = T, genDataFrame = F, overlap = 0.5)
 #MwStatistics(dat = TotalPiecewiseInterp, win = 2, TimeVarCol = 1, ColA = 2, ColB = 3, stat = "Slope", genplot = T, genDataFrame = F, overlap = 0.5)
 #MwStatistics(dat = TotalPiecewiseInterp, win = 2, TimeVarCol = 1, ColA = 2, ColB = 3, stat = "P-Value", genplot = T, genDataFrame = F, overlap = 0.5)
+
+
